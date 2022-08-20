a6a1abb026d0bfde2c692f43cefcbbe48e095fb6
rumaysaa <rumaysa.babulkhair.com>
2022-08-20 16:32:56 +0530

    functions

---
+++ test.py
@@ -0,0 +1,11 @@
+def mean(value):
+    if (type(value) == dict):
+        mean = (sum(value.values())) / (len(value))
+    else:
+        mean = sum(value) / len(value)
+    return mean
+list = [45,23,65]
+d = {"a":24,"b":11,"c":66}
+
+print(mean(list))
+print(mean(d))
