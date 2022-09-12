fdb6376ae66ebd593818493d2d2e2e4a2f988ba6
rumaysaa <rumaysa.babulkhair.com>
2022-09-12 22:47:15 +0530

    learninf pandas

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+''''
 def mean(value):
     if (type(value) == dict):
         mean = (sum(value.values())) / (len(value))
@@ -8,8 +8,14 @@ def mean(value):
 list = [45,23,65]
 d = {"a":24,"b":11,"c":66}
 
+
 print(mean(list))
 print(mean(d))
-
+'''''
+import pandas
+import os
+data = pandas.read_json('https://theblogosphere.herokuapp.com/articles/json')
+print(data)
+print("not found")
 
 
